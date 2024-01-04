254d6b3fc799ae17e68d0a4268356633de61da12
UrochomeRichy <kamererichard60@gmail.com>
2024-01-04 08:17:57 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this function takes no parameters and returns an integer
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
