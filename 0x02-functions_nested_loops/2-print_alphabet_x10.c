44b4c4594d04b819902b60eb5814c9f871c86a12
UrochomeRichy <kamererichard60@gmail.com>
2024-01-12 09:08:20 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - takes no parameters and no return value
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+	_putchar('\n');
+	}
+}
