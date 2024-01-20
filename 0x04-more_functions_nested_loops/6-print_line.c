1c7c5b8d9caa59ed4cb959551653102f9e3105c1
UrochomeRichy <kamererichard60@gmail.com>
2024-01-20 10:24:20 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - this function takes integer parameters and returns void
+ * @n: integer parameter of function print_line
+ * Return: void
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('-');
+		}
+		_putchar('\n');
+	}
+}
