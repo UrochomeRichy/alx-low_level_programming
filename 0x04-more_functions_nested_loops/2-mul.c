9940f5cb0588887ffca1f3063658702b1681fafa
UrochomeRichy <kamererichard60@gmail.com>
2024-01-19 23:58:30 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - this function takes an integer parameter and returns an integer
+ * @a: integer parameter of function mul
+ * @b: integer parameter of function mul
+ * Return: integer
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = (a * b);
+		_putchar(product);
+
+	_putchar('\n');
+	return (0);
+}
+
