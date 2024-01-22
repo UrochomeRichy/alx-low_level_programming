d5d619c44e8d73cddd162b19cbf96589d4d0e9a0
UrochomeRichy <kamererichard60@gmail.com>
2024-01-22 12:17:50 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - this function takes an integer parameter and returns void
+ * @a: integer parameter of function swap_int
+ * @b: integer function swap_int
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
