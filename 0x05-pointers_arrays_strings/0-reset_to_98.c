9c27ebf29dbbaa0136cde2b12652988b57bb1aee
UrochomeRichy <kamererichard60@gmail.com>
2024-01-22 12:00:00 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - this function returns void and takes an integer array as parameter.
+ * @*n: integer parameter aof function reset_to_98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
