c51b6314d7e9743d5b7f6498b708df3fe40886c4
UrochomeRichy <kamererichard60@gmail.com>
2024-01-25 06:26:24 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - this function takes character parameter and returns void
+ * @s: character parameter of function rev_string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\0')
+		counter++;
+	for (i = 0; i < counter / 2; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter - 1 - i];
+		s[counter - 1 - i] = rev;
+	}
+}
