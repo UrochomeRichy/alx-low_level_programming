91d388beff57b0981b9a743ad0e2647ee100c9c6
UrochomeRichy <kamererichard60@gmail.com>
2024-01-28 07:12:45 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,13 +6,15 @@
  */
 int is_prime_number(int n)
 {
+	int i;
+
 	if (n <= 1)
 	{
 		return (0);
 	}
 	else
 	{
-		for (int i = 0; i * i <= n; i++)
+		for (i = 2; i * i <= n; i++)
 		{
 			if (n % i == 0)
 			{
