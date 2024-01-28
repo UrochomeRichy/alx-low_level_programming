441f4896d04e1560f0e320a88663288d0b1f5317
UrochomeRichy <kamererichard60@gmail.com>
2024-01-28 07:04:48 -0800

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * is_prime_number -takes integer parameter and returns an integer.
+ * @n: integer parameter of function _is_prime_number.
+ * Return: integer
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else
+	{
+		for (int i = 0; i * i <= n; i++)
+		{
+			if (n % i == 0)
+			{
+				return (0);
+			}
+		}
+	}
+	return (1);
+}
